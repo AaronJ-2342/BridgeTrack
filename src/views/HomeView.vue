8bc7034879667b382ca3fff21aa75013f7f9e321
AaronJ-2342 <aaronjson27@gmail.com>
2026-01-19 20:08:31 -0500

    initial repo setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,82 @@
+<script setup></script>
+
+<template>
+  <main>
+    <img id="mainImg" src="@/assets/studentsStudying.jpg" alt="Students in class" />
+
+    <form id="formBox">
+      <h2>
+        Welcome to Bridge Track <br />
+        Prepritory school!
+      </h2>
+
+      <label class="Lbl">Username:</label>
+      <input type="text" id="name" placeholder="Username example" />
+      <!-- change this later -->
+
+      <label id="pswd_Lbl" class="Lbl">Password:</label>
+      <input type="text" id="pswd" />
+
+      <br /><br />
+      <input type="submit" value="Login" id="login" />
+
+      <a href="/register" alt="Create Account" id="act">Create Account</a>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+#mainImg {
+  object-fit: cover;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+#formBox {
+  background: white;
+  padding: 20px;
+  width: 350px;
+  border-radius: 8px;
+  box-shadow: 0 0 15px gray;
+  position: absolute;
+  top: 30%;
+  left: 38%;
+}
+
+#name {
+  border-radius: 8px;
+  font-size: 20px;
+}
+
+#pswd {
+  border-radius: 8px;
+  position: relative;
+  left: 3px;
+  top: 4px;
+  font-size: 20px;
+}
+
+#pswd_Lbl {
+  position: relative;
+  top: 4px;
+}
+
+#login {
+  border-radius: 8px;
+  font-size: 17px;
+  position: relative;
+  left: 280px;
+}
+
+#act {
+  position: relative;
+  right: 60px;
+  text-decoration: none;
+}
+
+.Lbl {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function returnHome() {
+  router.push('/')
+}
+</script>
+<template>
+  <img id="mainImg" src="@/assets/Forest.jpg" alt="Forest background" />
+
+  <form id="formBox">
+    <div class="main">
+      <h1 id="acnt">Create Account</h1>
+      <input class="innerMain" type="text" placeholder="First Name" />
+      <input class="innerMain" type="text" placeholder="Last Name" />
+      <input class="innerMain" type="text" placeholder="email" />
+      <input class="innerMain" type="text" placeholder="phone" />
+      <h3>
+        Are you 18 years or older? <span><input type="checkbox" /></span>
+      </h3>
+      <input class="innerMain" type="text" placeholder="birthdate" />
+      <!--Needs a calendar or a scroll menu with the given month days and years, hide on load and only show if the checkbox above is checked-->
+      <button @click="returnHome" id="return">Return</button>
+      <input type="submit" value="Create" id="create" />
+    </div>
+  </form>
+</template>
+
+<style scoped>
+#mainImg {
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+}
+
+#formBox {
+  background-color: white;
+  padding: 20px;
+  width: 400px;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: center;
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  box-shadow: 0 0 15px gray;
+
+  transform: translate(-50%, -50%);
+}
+
+#acnt {
+  display: flex;
+  justify-content: center;
+}
+
+#create {
+  border-radius: 8px;
+  font-size: 17px;
+  width: 120px;
+  position: relative;
+  left: 130px;
+  bottom: 20px;
+}
+
+#return {
+  border-radius: 8px;
+  font-size: 17px;
+  width: 120px;
+  position: relative;
+  top: 6px;
+}
+
+.main {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+
+.innerMain {
+  border-radius: 8px;
+  font-size: 20px;
+  position: relative;
+  margin-bottom: 6px;
+}
+</style>
